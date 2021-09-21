80da9c1b3e355e617bc6fd3a69a8a20c2653ae8b
jonrutter <contact@jonrutter.com>
2021-09-20 20:38:03 -0400

    Add fontsource font import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import '@fontsource/open-sans';
 
 ReactDOM.render(
   <React.StrictMode>
