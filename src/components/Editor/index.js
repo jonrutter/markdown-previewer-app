b4f821fcb71a4e6afbb347cffbcbdb0748c5a01d
jonrutter <contact@jonrutter.com>
2021-10-19 18:16:15 -0400

    Render Box component

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,44 +1,30 @@
 import React from 'react';
 
+// styles
 import * as s from './Editor.module.css';
 
-import { FaExpandAlt, FaCompressAlt } from 'react-icons/fa';
+// components
+import Box from '../Box';
 
 const Editor = ({ input, handleChange, fullScreen, setFullScreen }) => {
-  const toggleFullScreen = () => {
-    if (fullScreen) {
-      setFullScreen('');
-    } else {
-      setFullScreen('editor');
-    }
-  };
-  if (!fullScreen || fullScreen === 'editor') {
-    return (
-      <div
-        className={
-          fullScreen === 'editor' ? `${s.wrapper} ${s.expanded}` : s.wrapper
-        }
-      >
-        <section className={s.content}>
-          <header className={s.header}>
-            <h2 className={s.title}>Editor</h2>
-            <button onClick={toggleFullScreen}>
-              {fullScreen === 'editor' ? <FaCompressAlt /> : <FaExpandAlt />}
-            </button>
-          </header>
-          <textarea
-            className={s.editor}
-            id="editor"
-            onChange={(e) => handleChange(e.target.value)}
-          >
-            {input}
-          </textarea>
-        </section>
-      </div>
-    );
-  } else {
-    return null;
-  }
+  return (
+    <Box
+      title="Editor"
+      fullScreen={fullScreen}
+      setFullScreen={setFullScreen}
+      clear={() => handleChange('')}
+    >
+      <label htmlFor="editor" className="sr-only">
+        Enter markdown
+      </label>
+      <textarea
+        className={s.editor}
+        id="editor"
+        onChange={(e) => handleChange(e.target.value)}
+        value={input}
+      />
+    </Box>
+  );
 };
 
 export default Editor;
--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -5,9 +5,11 @@ import marked from 'marked';
 import DOMPurify from 'dompurify';
 import parse from 'html-react-parser';
 
+// styles
 import * as s from './Preview.module.css';
 
-import { FaExpandAlt, FaCompressAlt } from 'react-icons/fa';
+// components
+import Box from '../Box';
 
 marked.setOptions({
   gmf: true,
@@ -15,43 +17,18 @@ marked.setOptions({
 });
 
 const Preview = ({ input, fullScreen, setFullScreen }) => {
-  // const processedContent = input.replace('\n', '<br>');
   const content = marked(input);
   const clean = DOMPurify.sanitize(content, { USE_PROFILES: { html: true } });
 
-  const toggleFullScreen = () => {
-    if (fullScreen) {
-      setFullScreen('');
-    } else {
-      setFullScreen('preview');
-    }
-  };
-
-  if (!fullScreen || fullScreen === 'preview') {
-    return (
-      <div
-        className={
-          fullScreen === 'preview' ? `${s.wrapper} ${s.expanded}` : s.wrapper
-        }
-      >
-        <section className={s.content}>
-          <header className={s.header}>
-            <h2 className={s.title}>Previewer</h2>
-            <button onClick={toggleFullScreen}>
-              {fullScreen === 'preview' ? <FaCompressAlt /> : <FaExpandAlt />}
-            </button>
-          </header>
-          <div className={s.body}>
-            <div className={s.preview} id="preview">
-              {parse(clean)}
-            </div>
-          </div>
-        </section>
+  return (
+    <Box title="Preview" fullScreen={fullScreen} setFullScreen={setFullScreen}>
+      <div className={s.body}>
+        <div className={s.preview} id="preview">
+          {parse(clean)}
+        </div>
       </div>
-    );
-  } else {
-    return null;
-  }
+    </Box>
+  );
 };
 
 export default Preview;
